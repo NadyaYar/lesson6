61fe8631df4aa8c07bb78f777e4e9f9cfe316de4
NadyaYar <nadya.yarmak03@gmail.com>
2022-02-12 11:23:09 +0200

    Deal

---
+++ src/Deal.java
@@ -0,0 +1,43 @@
+import java.util.Date;
+
+public class Deal {
+    long id;
+    int price;
+    Date dateCreated;
+    boolean isConfirmed;
+    Date dateConfirmed;
+    String city;
+    String country;
+    String type;
+
+    public Deal(int price, Date dateCreated, boolean isConfirmed, Date dateConfirmed, String city, String country, String type) {
+        this.price = price;
+        this.dateCreated = dateCreated;
+        this.isConfirmed = isConfirmed;
+        this.dateConfirmed = dateConfirmed;
+        this.city = city;
+        this.country = country;
+        this.type = type;
+    }
+
+    public Deal() {
+    }
+
+    public void setConfirmOder() {
+        isConfirmed = true;
+        dateConfirmed = new Date();
+    }
+
+    public boolean checkPrice() {
+        if (price > 1000) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public boolean isValidType() {
+        return !(type != "buy" && type != "sale");
+    }
+}
+
